1627abed8e2aabd6d89fe1d77fe302a76a123c55
Marek Sedlak <bodka.zavinac@gmail.com>
2017-03-09 00:29:00 +0100

    M permissions + external storage

---
+++ pv239/cv3/app/src/main/java/cz/muni/pv239/marek/cv3/MainActivity.java
@@ -0,0 +1,156 @@
+package cz.muni.pv239.marek.cv3;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_WRITE_PERMISSION = 1001;
+    private static final String TAG = "CV3";
+    private Button mSaveButton;
+    private EditText mContentEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        prepareUi();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //
+            } else {
+                savingPermissionCheck();
+            }
+        }
+    }
+
+
+    private void prepareUi() {
+        mContentEditText = (EditText) findViewById(R.id.contentEditText);
+        mSaveButton = (Button) findViewById(R.id.saveButton);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savingPermissionCheck();
+            }
+        });
+    }
+
+    private void savingPermissionCheck() {
+
+        // runtime permissions start with the Marshmallow ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            // let's check whethere we already have the permission
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // android helper method to tell us if it's useful to show a hint
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    // show some alert explaining why it is important to grant the permission
+                    showSavingPermissionRationale(this);
+                } else {
+                    // just straight to the point
+                    requestPermissions(
+                            new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            REQUEST_WRITE_PERMISSION);
+                }
+            } else {
+                // Marshmallow or higher but the permissions are granted -> save the data
+                saveData();
+            }
+
+        } else {
+            // lower than Marshmallow -> permissions were granted during the install process
+            saveData();
+        }
+    }
+
+    private void showSavingPermissionRationale(final Context context) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Granting the permission needed");
+        alertBuilder.setMessage("Hi there, the app needs to write to the external storage to " +
+                "work properly. The content from the screen is stored there. " +
+                "Thank you for the understanding.");
+        alertBuilder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // this way you can get to the screen to set the permissions manually
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.fromParts("package", getPackageName(), null));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    private void saveData() {
+        try {
+            // the content
+            String content = mContentEditText.getText().toString();
+
+            // getting the root sd card directory
+            File root = ContextCompat.getExternalFilesDirs(this, null)[0];
+
+            // it may happen we do not have external memory available now
+            if (root == null) {
+                return;
+            }
+
+            File file = new File(root, "cv3.txt");
+
+            Log.i(TAG, file.getAbsolutePath());
+
+            // make sure the file exists
+            if (!file.exists()) {
+                Boolean created = file.createNewFile();
+                Log.i(TAG, created.toString());
+            }
+
+            // writing the data
+            FileWriter fileWritter = new FileWriter(file.getName(), true);
+            BufferedWriter bufferedWritter = new BufferedWriter(fileWritter);
+            bufferedWritter.write(content);
+            bufferedWritter.close();
+
+            // looks okay!
+            Toast.makeText(this, "Saved.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Not saved: " +
+                    e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
